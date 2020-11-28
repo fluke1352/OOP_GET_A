2d9c864991d8c1e6be91445f4032b35dd3b4046c
phawit-s <phawit.ps@gmail.com>
2020-11-28 20:38:44 +0700

    ส่ง

---
+++ Tuproject/src/KeyInput.java
@@ -0,0 +1,33 @@
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class KeyInput extends KeyAdapter{
+    Player p;
+
+    public KeyInput(Player p) {
+        this.p = p;
+    }
+    @Override
+    public void keyTyped(KeyEvent ke){
+    
+    }
+    @Override
+    public void keyPressed(KeyEvent ke){
+        p.keyPressed(ke);
+    }
+    public void keyReleased(KeyEvent ke){
+        p.keyReleased(ke);
+    }
+
+}
