bce52fb980b3b11dfe5da775d09b5b7d8de8ccef
w1nzix <56464459+w1nzix@users.noreply.github.com>
2020-12-06 21:14:01 +0700

    ใส่ hitbox และ เช็คดูว่าโดน hitbox ไหม

--- project/image.java
+++ project/image.java
@@ -9,41 +9,30 @@ package project;
  *
  * @author Fluke
  */
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.EOFException;
 import java.io.IOException;
 
 public class image {
-    public static BufferedImage punmap;
-    public static BufferedImage fluketomap;
-    public static BufferedImage tuumap;
-    public static BufferedImage ghosttop;
-    public static BufferedImage ghostright;
+    public static BufferedImage flukemap;
     public static BufferedImage ghostleft;
-    public static BufferedImage ghostdown;
-    public static BufferedImage selectmap_pun;
-    public static BufferedImage selectmap_fluke;
-    public static BufferedImage selectmap_tuu;
-    BufferedImage turn;
-    
-
+    public static BufferedImage ghostright;
+    public static BufferedImage ghosttop, flukeboss;
+    public static BufferedImage ghostdown, map, kame, bskame;
     
     public image(){
         try {
-//            ส่วนของการเลือกแมพ
-            selectmap_pun = ImageIO.read(getClass().getResource("image/selectmapPun.png"));
-            selectmap_fluke = ImageIO.read(getClass().getResource("image/selectmapFluke.png"));
-            selectmap_tuu = ImageIO.read(getClass().getResource("image/selectmapTuu.png"));
-            
-           fluketomap = ImageIO.read(getClass().getResource("image/fluketomap.png"));
-            punmap = ImageIO.read(getClass().getResource("image/punmap.png"));
-            tuumap = ImageIO.read(getClass().getResource("image/tuumap.png"));
-            ghosttop = ImageIO.read(getClass().getResource("image/top.png"));
-            ghostright = ImageIO.read(getClass().getResource("image/right.png"));
+//            System.out.println("A");
+            flukemap = ImageIO.read(getClass().getResource("image/fluketomap.png"));
             ghostleft = ImageIO.read(getClass().getResource("image/left.png"));
-            ghostdown = ImageIO.read(getClass().getResource("image/bottom.png"));
+            ghostright = ImageIO.read(getClass().getResource("image/right.png"));
+            ghosttop = ImageIO.read(getClass().getResource("image/top.png"));
+            ghostdown = ImageIO.read(getClass().getResource("image/down.png"));
+            map =  ImageIO.read(getClass().getResource("image/bg.png"));
+            flukeboss = ImageIO.read(getClass().getResource("image/Ultrainstinct.png"));
+            kame = ImageIO.read(getClass().getResource("image/kame.png"));
+            bskame = ImageIO.read(getClass().getResource("image/bskame.png"));
             
         } catch (Exception ex) {
             ex.printStackTrace();
